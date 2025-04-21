cd8d6ea1241e3ba982cc6b2afbaa40e1ba8d3126
chandan kumar <purshottamswain@gmail.com>
2025-04-22 00:13:01 +0530

    fixed require path to match user.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/userModel');
+const User = require('../models/user');
 
 // POST new user
 router.post('/add', async (req, res) => {
